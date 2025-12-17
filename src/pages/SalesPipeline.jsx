292121e0a1b49b87febf14d4cbe165a383e494ed
Target imports <targetimportsba@gmail.com>
2025-12-17 13:02:20 +0000

    File changes

--- src/pages/SalesPipeline.jsx
+++ src/pages/SalesPipeline.jsx
@@ -17,13 +17,6 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 export default function SalesPipeline() {
   const queryClient = useQueryClient();
